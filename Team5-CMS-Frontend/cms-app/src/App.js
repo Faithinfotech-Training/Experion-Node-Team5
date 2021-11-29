30c6b0f9141d40bbd11e7b41d10b2bfaeb99a7e6
rshav44 <95015906+rshav44@users.noreply.github.com>
2021-11-29 22:17:57 +0530

    Update App.js

--- Team5-CMS-Frontend/cms-app/src/App.js
+++ Team5-CMS-Frontend/cms-app/src/App.js
@@ -1,20 +1,8 @@
 
 import './App.css';
-
-import { Navbar, Container, Nav,Button} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Home from './Components/Home';
-import About from './Components/About';
-import Login from './Components/FrontOffice/Home';
-import NoMatch from './Components/NoMatch';
 import LabTechHome from './Components/Lab Technician/LabTechHome';
 import UploadTest from './Components/Lab Technician/UploadTest';
 import DaywiseList from './Components/Lab Technician/DaywiseList';
-
-import "./App.css";
-
-
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -71,6 +59,7 @@ function MyRouter() {
             <Nav.Link href='/about'>About Us</Nav.Link>
             <Nav.Link href='/login'>Login</Nav.Link>
             <Nav.Link href='/lab' >Laboratory</Nav.Link>
+ <Nav.Link href="/doctor">Doctor</Nav.Link>
             </Nav>
         </Container>
       </Navbar>
@@ -83,7 +72,13 @@ function MyRouter() {
         <Route path='/uploadtest' element={<UploadTest/>}/>
         <Route path='/daywiselist' element={<DaywiseList/>}/>
         <Route path='*' element={<NoMatch/>} />
-        
+          <Route path="/doctor" element={<DrDash />} />
+          <Route path="/appointment/:id" element={<Appointments />} />
+          <Route path="/appointmentdetails/:id" element={<AppointmentDetails />} />
+          <Route path="/addnotes/:id" element={<AddNotes />} />
+          <Route path="//labresults/:id" element={<LabResults />} />
+          <Route path="//medicalhistory/:id" element={<MedicalHistory />} />
+          <Route path="//patientdtails/:id" element={<PatientDetails />} />
       </Routes>
       
 
@@ -94,60 +89,6 @@ function MyRouter() {
       </footer>
     </Router>
 </>
-  )
-
-      <Router>
-        <h1>
-          <div
-            style={{
-              marginTop: 40,
-              padding: 10,
-              backgroundColor: "#5f0000",
-              color: "rgb(218, 153, 79)",
-            }}
-          >
-            {" "}
-            Health360
-          </div>
-        </h1>
-        <h6 style={({ marginTop: 30 }, { color: "rgb(218, 153, 79)" })}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua.{" "}
-        </h6>
-        <Navbar className="color-nav" variant="dark" style={{ marginTop: 60 }}>
-          <Container style={{ marginLeft: 0 }}>
-            <Navbar.Brand href="/">Health360</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/doctor">Doctor</Nav.Link>
-              <Nav.Link href="/about">About Us</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/doctor" element={<DrDash />} />
-          <Route path="/appointment/:id" element={<Appointments />} />
-          <Route path="/appointmentdetails/:id" element={<AppointmentDetails />} />
-          <Route path="/addnotes/:id" element={<AddNotes />} />
-          <Route path="//labresults/:id" element={<LabResults />} />
-          <Route path="//medicalhistory/:id" element={<MedicalHistory />} />
-          <Route path="//patientdtails/:id" element={<PatientDetails />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br /> <br />
-        <footer>
-          <p style={layLow}>Copyright - All Rights Reserved</p>
-        </footer>
-      </Router>
-    </>
   );
 
 }
