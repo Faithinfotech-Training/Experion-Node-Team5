c7e454acd9491b46be893589323e9380d0b53ffc
JobzzKJ <joblin.james@experionglobal.com>
2021-11-29 14:55:44 +0530

    Tables added

--- Team5-CMS-Backend/models/cms.js
+++ Team5-CMS-Backend/models/cms.js
@@ -1,21 +1,98 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
+const Sequelize = require("sequelize");
+const db = require("../config/database");
 
-const Role = db.define('role', {
-    RoleID: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    RoleName: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    Description: {
-        type: Sequelize.TEXT,
-        allowNull: false
-    }
+const Role = db.define("role", {
+  RoleID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  RoleName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  Description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
 
-})
+const Patients = db.define("patients", {
+  PatientsId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  DoctorId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  AppId: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  PatientName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  ContactNo: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
+const MedPres = db.define("medicinePrescribed", {
+  MedicinePrescriptionID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  MedicineId1: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+  MedicineId2: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+  MedicineId3: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+  MedicineId4: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+  Dosage: {
+    type: Sequelize.TEXT,
+    allowNull: true,
+  },
+});
+const Appointments = db.define("appointments", {
+  AppointmentId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  PatientId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  DoctorId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  AppointmentTime: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  ContactNo: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
 
 module.exports = Role;
+module.exports = Patients;
+module.exports = MedPres;
+module.exports = Appointments;
